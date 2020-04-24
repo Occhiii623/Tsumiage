9828278ad9d931c85bb94c3ddd3ab899e6454659
Mayumi <Kagura004@gmail.com>
2020-04-24 18:04:09 +0900

    setting default user icon
    - Enable default_url method in icon_uploader.
    - Create directories(/public/images/fallback)
    - Place 'defalut.jpg' in it.

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -19,9 +19,9 @@ class IconUploader < CarrierWave::Uploader::Base
   def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    "default.jpg"
+
   #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    "/images/fallback/" + [version_name, "default.jpg"].compact.join('_')
   end
 
   # Process files as they are uploaded:
