d42f1f8498974afa0333a3b3ba3ce23019c8723d
Mayumi <Kagura004@gmail.com>
2020-04-27 21:18:46 +0900

    Fix jquery load

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,19 +1,23 @@
-$(function(){
+$(document).on("turbolinks:load", function(){
 
-  $(".deleteIcon").each(function(i){
-    $(this).attr('id', 'item_' + (i+1));
+  $(function(){
+
+    $(".deleteIcon").each(function(i){
+      $(this).attr('id', 'item_' + (i+1));
+    });
+
+    $(".deleteBtn").each(function(i){
+      $(this).attr('id', 'btn_' + (i+1));
+    });
+
+    $(".deleteIcon").each(function(i){
+
+      i = i + 1
+      $("#item_" + i).on('click', function(){
+        $("#btn_" + i).toggle();
+      })
+
+    });
   });
 
-  $(".deleteBtn").each(function(i){
-    $(this).attr('id', 'btn_' + (i+1));
-  });
-
-  $(".deleteIcon").each(function(i){
-
-    i = i + 1
-    $("#item_" + i).on('click', function(){
-      $("#btn_" + i).toggle();
-    })
-
-  });
 });
