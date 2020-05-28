47f41cf05bda406c2ee7a5da047215976e74e26c
Mayumi <Kagura004@gmail.com>
2020-05-28 17:17:45 +0900

    Update uploader.rb

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -6,7 +6,7 @@ class IconUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   if Rails.env.development? || Rails.env.test?
-    strage :file
+    storage :file
   else
     storage :fog
   end
